a4cd3f60f47e73808dbb2f2bfe9bf288700dabf1
balaji096-ai <gurubalaji096@gmail.com>
2026-02-25 19:28:11 +0530

    Variable Program

---
+++ src/variables_Datatypes/PersonalDetails.java
@@ -0,0 +1,15 @@
+package variables_Datatypes;
+
+public class PersonalDetails {
+
+	public static void main (String[] args) {
+		String name ="Balaji";
+		int age =29;
+		Double experience = 4.3;
+		
+		System.out.println("Name: " + name);
+		System.out.println("Age: "+ age);
+		System.out.println("Experience: " + experience);
+		
+	} 	 	
+}
