7cb5a5a62d43513512b58b15403178d543bafc85
Balaji <balajiyalamandha@gmail.com>
2026-02-26 03:18:27 +0530

    first commit to push in ci/cd

--- src/variables_Datatypes/PersonalDetails.java
+++ src/variables_Datatypes/PersonalDetails.java
@@ -5,11 +5,17 @@ public class PersonalDetails {
 	public static void main (String[] args) {
 		String name ="Balaji";
 		int age =29;
-		Double experience = 4.3;
+		String designation = "Test Engineer";
+		double experience = 4.3;
+		boolean employed = true;
+		double salary =53000.00;
 		
 		System.out.println("Name: " + name);
 		System.out.println("Age: "+ age);
+		System.out.println("Designanation: " + designation);
 		System.out.println("Experience: " + experience);
+		System.out.println("Employed: " + employed);
+		System.out.println("Salary:" + salary);
 		
 	} 	 	
 }
