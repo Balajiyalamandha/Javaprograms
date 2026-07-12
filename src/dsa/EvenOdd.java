22f7203c585250ee36b9900f83df4ecd32a046de
Balaji <balajiyalamandha@gmail.com>
2026-07-12 23:09:15 +0530

    DSA_03

--- src/dsa/EvenOdd.java
+++ src/dsa/EvenOdd.java
@@ -2,4 +2,14 @@ package dsa;
 
 public class EvenOdd {
 
+	public static void main(String[] args) {
+
+        int number = 25;
+
+        if (number % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+	
 }
