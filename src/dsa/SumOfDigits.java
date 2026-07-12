0b1adcc9a6c73babe2c3ecb5f9cb38c050a1a1b4
Balaji <balajiyalamandha@gmail.com>
2026-07-12 23:21:57 +0530

    DSA_07

--- src/dsa/SumOfDigits.java
+++ src/dsa/SumOfDigits.java
@@ -2,4 +2,16 @@ package dsa;
 
 public class SumOfDigits {
 
+	public static void main(String[] args) {
+
+        int num = 12345;
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
 }
